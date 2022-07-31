70c2ea0551c6f2da6d900dc1f90efba72e3e7dc0
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-07-31 14:29:35 +0200

    Change max line length to 88

--- tests/test_bag.py
+++ tests/test_bag.py
@@ -2,7 +2,8 @@ def test_bags(bags):
     msgs = [
         "Bag 1 (ItemType.STANDARD - 20)",
         '1: "bag": 1, "slot": 1, "id": 2, "stack": 3, "max_stack": 10, "type": SOUL',
-        '2: "bag": 1, "slot": 2, "id": 3, "stack": 15, "max_stack": 20, "type": ENCHANTING',
+        '2: "bag": 1, "slot": 2, "id": 3, "stack": 15, "max_stack": 20,'
+        ' "type": ENCHANTING',
         '3: "bag": 1, "slot": 3, "id": 2, "stack": 8, "max_stack": 10, "type": SOUL',
         '5: "bag": 1, "slot": 5, "id": 1, "stack": 1, "max_stack": 1, "type": STANDARD',
         '9: "bag": 1, "slot": 9, "id": 1, "stack": 1, "max_stack": 1, "type": STANDARD',
